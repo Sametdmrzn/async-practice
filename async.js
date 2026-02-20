2f9c54179322cfd271d22e1f8164c78a1dbcca61
Samet DEMİREZEN <sametdmzn@gmail.com>
2026-02-20 14:30:28 +0300

    real error checking

--- async.js
+++ async.js
@@ -1,26 +1,32 @@
 async function getData() {
-  console.log("1) İstekler başlatılıyor...");
+  console.log("1: Birden fazla istek gönderiliyor.");
 
-  try {
-    const [userResponse, postResponse] = await Promise.all([
-      fetch("https://jsonplaceholder.typicode.com/users/1"),
-      fetch("https://jsonplaceholder.typicode.com/posts/1"),
-    ]);
+  const userReq = fetch("https://jsonplaceholder.typicode.com/users/1");
+  const postReq = fetch("https://jsonplaceholder.typicode.com/posts/1");
+  const wrongReq = fetch("https://jsonplaceholder.typicode.com/wrong-url");
 
-    if (!userResponse.ok || !postResponse.ok) {
-      throw new Error("API isteklerinden biri başarısız oldu");
+  const resaults = await Promise.allSettled([userReq, postReq, wrongReq]);
+
+  console.log("2: Sonuçlar");
+
+  for (let i = 0; i < resaults.length; i++) {
+    const result = resaults[i];
+
+    if (result.status === "fulfilled") {
+      const response = result.value;
+
+      if (!Response.ok) {
+        console.log(`${i + 1}. istek HTTP hatası: ${response.status}`);
+      } else {
+        console.log(
+          `${i + 1}. istek başarıyla tamamlandı (${response.status})`,
+        );
+      }
+    } else {
+      console.log(` ${i + 1}. istek  REJECT oldu:`, result.reason.message);
     }
-
-    const user = await userResponse.json();
-    const post = await postResponse.json();
-
-    console.log("2) Kullanıcı verisi:", user);
-    console.log("3) Gönderi verisi:", post);
-  } catch (err) {
-    console.log("4) Hata:", err.message);
   }
-
-  console.log("5) İşlem tamamlandı");
+  console.log("3: işlem tamamlandı");
 }
 
 getData();
